e9a05de779b69ec4651945418dcbb64be3dd22c2
Nerotheum <nerotheum@gmail.com>
2025-01-30 22:04:37 +0100

    Create basic bot that can login

---
+++ Bot.py
@@ -0,0 +1,12 @@
+import discord
+intents = discord.Intents.default()
+intents.message_content = True
+bot = discord.Client(intents=intents)
+@bot.event
+async def on_ready():
+    print(f'Logged in as: {bot.user}')
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+bot.run('token')
