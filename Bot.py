be3bcf328e0ed00d1f26cd94b42414d11cb4dc60
Nerotheum <nerotheum@gmail.com>
2025-01-31 16:23:19 +0100

    Format code, read token from .env

--- Bot.py
+++ Bot.py
@@ -1,12 +1,22 @@
+import os
+from dotenv import load_dotenv
 import discord
+
+load_dotenv()
+
+token = os.getenv("TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
+
 bot = discord.Client(intents=intents)
+
 @bot.event
 async def on_ready():
     print(f'Logged in as: {bot.user}')
+
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
-bot.run('token')
+
+bot.run(token)
