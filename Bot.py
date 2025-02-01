bb1c4d95da632d98e9d1deb79994b1bd2f668e3e
Nerotheum <nerotheum@gmail.com>
2025-02-01 11:31:58 +0100

    Define and read command prefix from .env

--- Bot.py
+++ Bot.py
@@ -1,14 +1,15 @@
 import os
 from dotenv import load_dotenv
 import discord
+from discord.ext import commands
 
 load_dotenv()
-
 token = os.getenv("TOKEN")
+prefix = os.getenv("PREFIX")
+
 intents = discord.Intents.default()
 intents.message_content = True
-
-bot = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix=prefix, intents=intents)
 
 @bot.event
 async def on_ready():
